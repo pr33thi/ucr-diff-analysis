ca8864b02de98f9618a9a61a021a3c6611e594b3
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2019-02-06 12:34:42 -0500

    Reformat and change the heuristics

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import csv
 
 
 def parse_args():
@@ -20,42 +21,32 @@ def parse_diff_file(args):
     return full_diff
 
 def analyze_ucr_diff(full_diff):
+    for diff_line in full_diff:
+        completely_different = is_completely_different(diff_line)
+        if not completely_different:
+            i_total_records_diff = get_i_total_records_diff(diff_line)
+            total_row_diff = get_total_row_diff(diff_line)
+        dummy = 5
 
-    competely_different_indices = []
-    i_total_records_diff_indices = []
-    very_high_value_indices = []
-    has_nulls_indices = []
+def is_completely_different(diff_line):
+    if diff_line[0][0] == []:
+        return True
 
-    very_high_value = 4
+def get_i_total_records_diff(diff_line):
+    if diff_line[1][0][0] == 'iTotalRecords':
+        return diff_line[1][0][1]
 
-    for index, log_line in enumerate(full_diff):
-            # If the [0][0] index is an empty list, then the inputs were completely different
-            if not full_diff[0][0]:
-                competely_different_indices.append(index)
-            else:
-                for diff_entry in full_diff:
-                    # If there was any difference in iTotalRecords
-                    if diff_entry[0][0] == 'iTotalRecords':
-                        i_total_records_diff_indices.append(index)
-                        break
-                    if not isinstance(diff_entry[1], str):
-                        has_nulls_indices.append(index)
-                    elif float(diff_entry[1]) > very_high_value:
-                        very_high_value_indices.append(index)
-
-    print("competely_different_indices: {}".format(competely_different_indices))
-    print("i_total_records_diff_indices: {}".format(i_total_records_diff_indices))
-    print("very_high_value_indices: {}".format(very_high_value_indices))
-    print("has_nulls_indices: {}".format(has_nulls_indices))
-
-
-    # Heuristics
-
-    # len(diff)
-    # another could be `iTotalRecords` being different,
-    # another could be there being a large difference between one of the columns, like the candidate has a value of 1 and the control has a value of 1000
+def get_total_row_diff(diff_line):
+    if diff_line[-1][0][0] == 'total_row':
+        return diff_line[-1]
 
 if __name__=="__main__":
     args = parse_args()
     full_diff = parse_diff_file(args)
+
+    only_some_diff = []
+    for diff_line in full_diff:
+        if not is_completely_different(diff_line):
+            only_some_diff.append(diff_line)
+
     analyze_ucr_diff(full_diff)
